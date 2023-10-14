6bf1be086c8cdc2df4391392f225243b7131f5d3
ahmedbentabet <bentabet841@gmail.com>
2023-10-14 14:24:27 +0000

    Debugging

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = 0
-    max_key = ""
     if a_dictionary is None:
         return None
+
+    max_key = next(iter(a_dictionary))
+    max_value = a_dictionary[max_key]
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
             max_key = key
 
     return max_key
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
+
+
