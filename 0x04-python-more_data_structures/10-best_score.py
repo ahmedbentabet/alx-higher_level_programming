d39384e49aeb321c8664ec7ac1caf5e0c9afa467
ahmedbentabet <bentabet841@gmail.com>
2023-10-14 13:49:09 +0000

    Debugging

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_value = 0
     max_key = ""
-    if a_dictionary is None:
+    if a_dictionary == {}:
         return None
     for key in a_dictionary:
         if a_dictionary[key] > max_value:
