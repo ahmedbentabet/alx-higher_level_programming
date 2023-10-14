b8a69c9d0aeabe5fd20c301078b35b43d709486e
ahmedbentabet <bentabet841@gmail.com>
2023-10-14 16:51:02 +0000

    Add task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int = 0
+    for i in range(len(roman_string) - 1):
+        if my_dict[roman_string[i]] >= my_dict[roman_string[i + 1]]:
+            int += my_dict[roman_string[i]]
+        elif my_dict[roman_string[i]] < my_dict[roman_string[i + 1]]:
+            int -= my_dict[roman_string[i]]
+    int += my_dict[roman_string[-1]]
+    return int
