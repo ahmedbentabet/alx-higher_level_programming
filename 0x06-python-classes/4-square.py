8fe843d1db1b786c616cf7d1bf86d984e042664b
ahmedbentabet <bentabet841@gmail.com>
2023-10-24 21:54:04 +0000

    Add task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" a class Square that represents a square. """
+"""This module defines a class Square that represents a square."""
 
 
 class Square:
-    """ A Square with a private instance attribute size. """
+    """ A class named Square with a private instance attribute size. """
 
     def __init__(self, size=0):
         """
@@ -12,9 +12,42 @@ class Square:
         Parameters:
             size (int): The size of the square.
         """
-        if not isinstance(size, int):
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Parameters:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square.
+
+        Returns:
+            int: The area of the square (size * size).
+        """
+        return self.__size * self.__size
