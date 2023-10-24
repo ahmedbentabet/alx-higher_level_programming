d9541c6f7643c8273cb968871b690e7544794791
ahmedbentabet <bentabet841@gmail.com>
2023-10-24 21:28:22 +0000

    Fix task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
+""" This is an empty class Square that defines a square. """
+
+
 class Square:
+    """ This class doesn't have any attributes or methods defined. """
     pass
