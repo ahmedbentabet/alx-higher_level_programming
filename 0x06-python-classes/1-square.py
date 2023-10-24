3850f7a0156cea134944c77e739e8988570d7963
ahmedbentabet <bentabet841@gmail.com>
2023-10-24 21:33:43 +0000

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module defines a class Square that represents a square."""
+
+class Square:
+    """A class named Square with a private instance attribute size."""
+
+    def __init__(self, size):
+        """
+        Initializes the Square instance.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
