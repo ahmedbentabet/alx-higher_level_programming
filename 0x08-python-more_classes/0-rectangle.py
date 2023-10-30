8a5e1ad87eb0495dbd62558430f472d6c2d2623b
ahmedbentabet <bentabet841@gmail.com>
2023-10-30 18:58:40 +0000

    Fix: add docstrings

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """A class representing a rectangle.add"""
+    """A class representing a rectangle"""
     def __init__(self):
         """Initializes a Rectangle instance"""
         pass
