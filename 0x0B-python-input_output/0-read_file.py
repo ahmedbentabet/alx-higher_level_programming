76cc5239888231b7a6ebca38368d0ccf4c304331
ahmedbentabet <bentabet841@gmail.com>
2023-11-07 17:13:08 +0000

    Fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ Read a text file (UTF8) and prints it to stdout """
 
-    with open(filename, "r", encoding="utf-8") as myFile:
-        print(myFile.read())
+    with open(filename, "r", encoding="utf-8") as my_file:
+        print(my_file.read())
