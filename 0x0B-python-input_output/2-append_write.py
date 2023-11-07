30e0d9031ab905692d86ac962e241307f469c9ca
ahmedbentabet <bentabet841@gmail.com>
2023-11-07 17:41:30 +0000

    Fix task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """ Append a string to a text file and returns the num of chars written. """
+    """ Append a string to a text file & returns the num of chars written. """
 
     num_of_char = 0
     with open(filename, "a", encoding="utf-8") as my_file:
