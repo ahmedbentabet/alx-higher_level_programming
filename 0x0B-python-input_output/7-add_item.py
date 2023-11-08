fac96d3c8e583067ae196f055111746e0b41660a
ahmedbentabet <bentabet841@gmail.com>
2023-11-08 14:46:20 +0000

    Fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,6 @@ new_list = []
 for arg in args:
     new_list.append(arg)
 
-list_from_json = load_from_json_file("add_item.json")
-save_to_json_file(new_list[1:], "add_item.json")
-list_from_json.extend(new_list[1:])
-save_to_json_file(list_from_json, "add_item.json")
+items = load_from_json_file("add_item.json")
+items.extend(new_list[1:])
+save_to_json_file(items, "add_item.json")
