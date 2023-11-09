3adba12dd5ccb2b346cdb92055e4b0dbd91c87ee
ahmedbentabet <bentabet841@gmail.com>
2023-11-09 18:08:16 +0000

    Update task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-
-"""A class Square that represents a square."""
+"""Define the Squire class"""
 
 
 class Square:
-    """ Square with a private instance attribute size. """
+    """A Square with a private instance attribute."""
 
     def __init__(self, size=0):
         """
-        Initializes the Square instance.
-
-        Parameters:
-            size (int): The size of the square.
+        Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
-        if not isinstance(size, int):
+        self.__size = size
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
 
     def area(self):
-        """
-        Returns the area of the square.
-
-        Returns:
-            int: The area of the square (size * size).
-        """
         return self.__size * self.__size
