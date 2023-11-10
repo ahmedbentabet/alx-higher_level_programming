0dd82e4368dc88531b5eea951b828c67fb67265f
ahmedbentabet <bentabet841@gmail.com>
2023-11-10 17:47:13 +0000

    Fix: pycodestyle in task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,5 +40,5 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range (self.__size):
+            for i in range(self.__size):
                 print("#" * self.__size)
