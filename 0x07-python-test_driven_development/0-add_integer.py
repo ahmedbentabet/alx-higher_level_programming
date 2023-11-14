c8c80aba4a84b23239ce4d8c30d0c3dc06193866
ahmedbentabet <bentabet841@gmail.com>
2023-11-14 14:50:05 +0000

    Add task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,21 @@
-a = ()
-b = ()
+#!/usr/bin/python3
+"""
 
-print(a is b )
+The addition module.
 
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Add two integers
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+print(add_integer(None))
