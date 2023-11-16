eda97208700ef8659a14e426715c7148c8402948
ahmedbentabet <bentabet841@gmail.com>
2023-11-16 17:05:43 +0000

    Fix task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """A child class of the 'list' class."""
 
-    def __init__(self):
-        list.__init__(self)
-
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
