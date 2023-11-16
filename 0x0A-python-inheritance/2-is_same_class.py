a8ccb22feae136ec44281ee0fd1f9d4284754052
ahmedbentabet <bentabet841@gmail.com>
2023-11-16 17:37:23 +0000

    Fix task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """Check instantiation."""
 
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
