4378b65a2403aeffb053f1102c2b5161cf42379d
ahmedbentabet <bentabet841@gmail.com>
2023-11-16 18:40:32 +0000

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class representing a BaseGeometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
