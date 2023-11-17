4d8c3d8759b32b3a36d7780ed31804d4c0e21c3c
ahmedbentabet <bentabet841@gmail.com>
2023-11-17 10:20:39 +0000

    Update task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,10 +9,5 @@ class Square(Rectangle):
 
     def __init__(self, size):
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return (self.__size * self.__size)
-
-    def __str__(self):
-        return f"[Rectangle] {self.__size}/{self.__size}"
