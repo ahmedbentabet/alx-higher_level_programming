e65dbbf964591a7d5b1e88c52547efd8a3abd449
ahmedbentabet <bentabet841@gmail.com>
2023-11-17 09:48:20 +0000

    Add task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a class Rectangle that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """"A class that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
