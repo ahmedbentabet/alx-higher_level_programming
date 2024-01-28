67c0aee4883e262dfd1723a128cf4512056884b2
Ahmed Bentabet <bentabet841@gmail.com>
2024-01-28 12:31:12 +0000

    Add task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define The addition function."""
+
+
+def add_integer(a, b=98):
+    """Add two integers"""
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
