c962dc78169c73097aafc47c6a367e4f6967e5a3
Ahmed Bentabet <bentabet841@gmail.com>
2024-01-29 21:55:49 +0000

    Update task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
         if self.width * self.height == 0:
             return ''
         else:
-            return "\n".join((str(self.print_symbol) * self.width) for _ in range(self.height))
+            return "\n".join(str(self.print_symbol) * self.width for _ in range(self.height))
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self. height)
