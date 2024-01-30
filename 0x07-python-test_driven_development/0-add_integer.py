973a67b0c2562966332ca5289af823d7d907ca3f
Ahmed Bentabet <bentabet841@gmail.com>
2024-01-30 22:43:37 +0000

    Change type() with isinstance()

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,9 +11,10 @@ def add_integer(a, b=98):
     Add two integers
     """
 
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+
