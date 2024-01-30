b6484a0dc8a27b6f624a2874c49f27e6e30ae16f
Ahmed Bentabet <bentabet841@gmail.com>
2024-01-30 23:06:31 +0000

    Add task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Print square."""
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print('#' * size)
