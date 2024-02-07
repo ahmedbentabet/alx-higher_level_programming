2194269ee9ffddbfdf0e3877b47bb4c2a0da23ec
ahmedbentabet <bentabet841@gmail.com>
2024-02-07 12:32:40 +0000

    Revert "Add some comments"
    This reverts commit 6cc7ead1721f59daf0d9f4ceb0904d5278669575.

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""A "Class to JSON" module."""
+"""fdfsd"""
 
 
 def class_to_json(obj):
-    """
-    Convert the attrs of a class instance to a dict suitable for JSON.
+    """fsdf"""
 
-    Args:
-        obj: An instance of a class.
-
-    Returns:
-        dict: A dictionary containing the attributes of the class instance.
-    """
     dict_description = {}
 
-    # Iterate through the attributes of the object
     for attr_name in dir(obj):
-        # Get the value of the attribute
         attr_value = getattr(obj, attr_name)
-
-        # Exclude callable objects and special attributes/methods
         if not callable(attr_value) and not attr_name.startswith("__"):
             dict_description[attr_name] = attr_value
-
     return dict_description
