1691b8471656dc97f4e5a88914879a11e401a060
bentabet1 <dooms1516@gmail.com>
2024-02-07 10:58:59 +0000

    Fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,16 +6,14 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
+args = sys.argv[1:]
 new_list = []
-for arg in args:
-    new_list.append(arg)
-
+new_list.extend(args)
 
 if os.path.isfile("add_item.json"):
     items = load_from_json_file("add_item.json")
 else:
     items = []
 
-items.extend(new_list[1:])
+items.extend(new_list)
 save_to_json_file(items, "add_item.json")
