5b1bcfecef3cc70ac5c4b61ca9053e2ade0088fc
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 14:43:14 +0000

    Fix pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,4 +76,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return a string representation of the Rectangle."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return (
+            f"[{self.__class__.__name__}] ({self.id}) " 
+            f"{self.x}/{self.y} - {self.width}/{self.height}"
+        )
