013e363ebcd0d8099b88c21197fd63906a2fc933
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 10:18:33 +0000

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,3 +19,35 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r1.id, 1)
         self.assertEqual(self.r2.id, 2)
         self.assertEqual(self.r3.id, 12)
+
+    def test_width_validation(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle("invalid", 2)
+        with self.assertRaises(ValueError):
+            r = Rectangle(0, 2)
+
+    def test_height_validation(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, "invalid")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 0)
+
+    def test_x_validation(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, "invalid")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2, -1)
+
+    def test_y_validation(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, 0, "invalid")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2, 0, -1)
+
+    def test_valid_values(self):
+        r = Rectangle(10, 2, 0, 0, 1)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        self.assertEqual(r.id, 1)
