3578e6cde211520b98569c37b610cb9e4c9e6e0c
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 16:32:12 +0000

    Add tsk 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""efergfre"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """fdfsdf"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, x, y, width, height)
+        self.size = width
+
+    def __str__(self):
+        """Return a string representation of the Square."""
+        return (
+            f"[{self.__class__.__name__}] ({self.id}) "
+            f"{self.x}/{self.y} - {self.size}"
+        )
