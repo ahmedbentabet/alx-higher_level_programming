f49a957c1bf3612b44cab80f21440231a0b3972f
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 11:40:16 +0000

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,19 +6,19 @@ from models.base import Base
 class TestBase(unittest.TestCase):
 
     def setUp(self):
-        Base._Base__nb_objects = 0  # Explicitly reset __nb_objects to 0 before each test
+        pass  # Explicitly reset __nb_objects to 0 before each test
 
     def tearDown(self):
         pass
 
     def test_id(self):
         self.b1 = Base()
-        self.b2 = Base()
-        self.b3 = Base(12)
-        self.b4 = Base()
         self.assertEqual(self.b1.id, 1)
+        self.b2 = Base()
         self.assertEqual(self.b2.id, 2)
+        self.b3 = Base(12)
         self.assertEqual(self.b3.id, 12)
+        self.b4 = Base()
         self.assertEqual(self.b4.id, 3)
 
 """         self.emp_1.first = 'John'
