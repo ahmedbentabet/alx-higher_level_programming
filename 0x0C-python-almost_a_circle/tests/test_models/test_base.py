e4156adaafd4d42717912a26ac5c70a8008700c3
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 12:17:15 +0000

    Update the tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,53 +14,12 @@ class TestBase(unittest.TestCase):
     def test_id(self):
         self.b1 = Base()
         self.assertEqual(self.b1.id, 1)
+
         self.b2 = Base()
         self.assertEqual(self.b2.id, 2)
+
         self.b3 = Base(12)
         self.assertEqual(self.b3.id, 12)
+
         self.b4 = Base()
-        self.assertEqual(self.b4.id, 3)
-
-"""         self.emp_1.first = 'John'
-        self.emp_2.first = 'Jane'
-
-        self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
-        self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
-
-    def test_fullname(self):
-        print('test_fullname')
-        self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
-        self.assertEqual(self.emp_2.fullname, 'Sue Smith')
-
-        self.emp_1.first = 'John'
-        self.emp_2.first = 'Jane'
-
-        self.assertEqual(self.emp_1.fullname, 'John Schafer')
-        self.assertEqual(self.emp_2.fullname, 'Jane Smith')
-
-    def test_apply_raise(self):
-        print('test_apply_raise')
-        self.emp_1.apply_raise()
-        self.emp_2.apply_raise()
-
-        self.assertEqual(self.emp_1.pay, 52500)
-        self.assertEqual(self.emp_2.pay, 63000)
-
-    def test_monthly_schedule(self):
-        with patch('employee.requests.get') as mocked_get:
-            mocked_get.return_value.ok = True
-            mocked_get.return_value.text = 'Success'
-
-            schedule = self.emp_1.monthly_schedule('May')
-            mocked_get.assert_called_with('http://company.com/Schafer/May')
-            self.assertEqual(schedule, 'Success')
-
-            mocked_get.return_value.ok = False
-
-            schedule = self.emp_2.monthly_schedule('June')
-            mocked_get.assert_called_with('http://company.com/Smith/June')
-            self.assertEqual(schedule, 'Bad Response!') """
-
-
-""" if __name__ == '__main__':
-    unittest.main() """
+        self.assertEqual(self.b4.id, 3)  # 2 + 1 = 3
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,10 +14,12 @@ class TestRectangle(unittest.TestCase):
 
     def test_id(self):
         self.r1 = Rectangle(10, 2)
-        self.r2 = Rectangle(2, 10)
-        self.r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(self.r1.id, 1)
+
+        self.r2 = Rectangle(2, 10)
         self.assertEqual(self.r2.id, 2)
+
+        self.r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(self.r3.id, 12)
 
     def test_width_validation(self):
