11b50d36d700f055c183f57a90e34af35afe0ef7
ahmedbentabet <bentabet841@gmail.com>
2024-02-13 11:34:06 +0000

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,14 +8,14 @@ class TestRectangle(unittest.TestCase):
 
     def setUp(self):
         Base._Base__nb_objects = 0  # Explicitly reset __nb_objects to 0 before each test
-        self.r1 = Rectangle(10, 2)
-        self.r2 = Rectangle(2, 10)
-        self.r3 = Rectangle(10, 2, 0, 0, 12)
 
     def tearDown(self):
         pass
 
     def test_id(self):
+        self.r1 = Rectangle(10, 2)
+        self.r2 = Rectangle(2, 10)
+        self.r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(self.r1.id, 1)
         self.assertEqual(self.r2.id, 2)
         self.assertEqual(self.r3.id, 12)
