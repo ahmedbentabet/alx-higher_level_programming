c54bae2d188de5935e26c9b2857c5f38dace7d7f
ahmedbentabet <bentabet841@gmail.com>
2024-02-20 12:25:02 +0000

    Add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines the Base class."""
+import json
 
 
 class Base:
     """the Base class."""
 
     __nb_objects = 0
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            json_string = json.dumps(list_dictionaries)
+            return json_string
 
     def __init__(self, id=None):
         if id is None:
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -24,3 +24,33 @@ class TestBase(unittest.TestCase):
 
         self.b4 = Base()
         self.assertEqual(self.b4.id, 3)  # 2 + 1 = 3
+
+    def test_to_json_string_empty(self):
+        # Call the to_json_string method with an empty list
+        result = Base.to_json_string([])
+
+        # Define the expected result
+        expected_result = "[]"
+
+        # Compare the result with the expected string
+        self.assertEqual(result, expected_result)
+
+    def test_to_json_string_none(self):
+        # Call the to_json_string method with None
+        result = Base.to_json_string(None)
+
+        # Define the expected result
+        expected_result = "[]"
+
+        # Compare the result with the expected string
+        self.assertEqual(result, expected_result)
+
+    def test_to_json_string_non_empty(self):
+        # Call the to_json_string method with a non-empty list of dictionaries
+        result = Base.to_json_string([{'id': 1, 'name': 'med'}, {'id': 2, 'name': 'Jane'}])
+
+        # Define the expected JSON string
+        expected_result = '[{"id": 1, "name": "med"}, {"id": 2, "name": "Jane"}]'
+
+        # Compare the result with the expected JSON string
+        self.assertEqual(result, expected_result)
