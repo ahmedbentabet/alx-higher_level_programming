e46a321dd7ba7c743e0bf3e555efba156dbdb8f2
ahmedbentabet <bentabet841@gmail.com>
2024-02-21 07:10:30 +0000

    Add some files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,27 @@ class Base:
             json_string = json.dumps(list_dictionaries)
             return json_string
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_objs = []
+        file_name = f"{cls.__name__}.json"
+
+        with open(file_name, "w") as file:
+                json_string = cls.to_json_string()
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_objs = []
+        file_name = f"{cls.__name__}.json"
+        
+        with open(file_name, 'w') as file:
+            json_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            file.write(json_str)
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
