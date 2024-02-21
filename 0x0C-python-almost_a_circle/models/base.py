e15f60ae1b752dd9c0d312c26f8ebd419ce283ad
ahmedbentabet <bentabet841@gmail.com>
2024-02-21 14:52:26 +0000

    Add task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,6 +11,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries."""
+
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
@@ -20,6 +21,7 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file."""
+
         if list_objs is None:
             list_objs = []
         file_name = f"{cls.__name__}.json"
@@ -32,6 +34,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string."""
+
         if json_string is None or len(json_string) == 0:
             python_list = []
         else:
@@ -39,6 +42,32 @@ class Base:
             python_list = json.loads(json_string)
 
         return python_list
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set."""
+
+        # Create a dummy instance with mandatory attributes
+        dummy_instance = cls(1, 1)
+        # Use the update method to apply real values from dictionary
+        dummy_instance.update(**dictionary)
+
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances loaded from a JSON file."""
+        filename = f"{cls.__name__}.json"
+        try:
+            with open(filename, 'r') as file:
+                list_of_dict = json.load(file)
+        except FileNotFoundError:
+            # If the file doesn't exist, return an empty list
+            return []
+
+        # Use from_json_string and create methods to convert dictionaries to instances
+        list_instances = [cls.create(**dict) for dict in list_of_dict]
+        return list_instances
 
     def __init__(self, id=None):
         if id is None:
