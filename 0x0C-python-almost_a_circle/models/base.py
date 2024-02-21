73084508366d2c67da833b02e42f294c905e66aa
ahmedbentabet <bentabet841@gmail.com>
2024-02-21 18:39:06 +0000

    Add task 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines the Base class."""
 import json
+import csv
 
 
 class Base:
@@ -73,6 +74,50 @@ class Base:
         # Use from_json_string and create methods to convert dicts to instances
         list_instances = [cls.create(**dict) for dict in list_of_dict]
         return list_instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes a list of objects to a CSV file."""
+        filename = cls.__name__ + ".csv"
+
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([
+                        obj.id, obj.width, obj.height,
+                        obj.x, obj.y
+                        ])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes objects from a CSV file."""
+        filename = cls.__name__ + ".csv"
+        obj_list = []
+
+        try:
+            with open(filename, mode='r') as file:
+                reader = csv.reader(file)
+
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        obj_list.append(cls(
+                            int(row[0]), int(row[1]), int(row[2]),
+                            int(row[3]), int(row[4])
+                            ))
+                    elif cls.__name__ == "Square":
+                        obj_list.append(cls(
+                            int(row[0]), int(row[1]),
+                            int(row[2]), int(row[3])
+                            ))
+
+            return obj_list
+
+        except FileNotFoundError:
+            return obj_list
 
     def __init__(self, id=None):
         if id is None:
