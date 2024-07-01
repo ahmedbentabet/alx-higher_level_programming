d9bea80d75752a7d50b5cb1dcfabe0cc667ba0c5
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-01 18:55:02 +0000

    Fix: pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     
-    # Get the letter from the command line argument, or set q="" if no argument is given
+    # Get the letter from the command line argument
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else: 
