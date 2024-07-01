48716a31291ffcf866a7f09627882ebd75e99a68
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-01 19:32:59 +0000

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Uses GitHub API to display user id using Basic Authentication
+with a personal access token.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    # Send a GET request to the GitHub API with Basic Authentication
+    response = requests.get(url, auth=(username, token))
+
+# Parse the JSON response
+json_response = response.json()
+# Print the user id if available, otherwise print None
+print(json_response.get("id"))
