7fb043772656a08cc79749e3ff615eb962a461a1
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-01 17:18:01 +0000

    Add task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email as a parameter and displays the
+body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Prepare the payload for the POST request
+    data = {'email': email}
+
+    # Send a POST request to the URL with the email as a parameter
+    response = requests.post(url, data=data)
+
+    # Display the body of the response (decoded in utf-8)
+    print(response.text)
