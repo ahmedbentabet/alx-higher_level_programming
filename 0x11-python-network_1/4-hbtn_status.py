052684b4d9c7e0ae9bc232132047bc8ddbd02272
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-01 15:17:31 +0000

    Add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Fetches the status from https://alx-intranet.hbtn.io/status using requests.
+
+Displays information about the response including type and content.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(content).__name__}")
+    print(f"\t- content: {content}")
