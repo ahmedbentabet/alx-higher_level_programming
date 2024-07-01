45d1a3a848792399284bb46cecd3fbfb850f9070
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-01 17:26:26 +0000

    Add task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response.
+If the HTTP status code is >= 400, prints Error code: followed
+by the status code.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the status code is >= 400
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        # Print the body of the response
+        print(response.text)
